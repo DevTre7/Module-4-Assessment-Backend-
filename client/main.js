b5c645957d993b2a37a2b4f2f7fdc6f27251d973
Emmanuel Dada <Emmanuel.dada71@gmail.com>
2023-01-08 23:23:28 -0600

    all my apis are currently woring now

--- client/main.js
+++ client/main.js
@@ -1,64 +1,63 @@
-const complimentBtn = document.getElementById("complimentButton")
+const complimentBtn = document.getElementById("complimentButton");
 
 const getCompliment = () => {
-    axios.get("http://localhost:4000/api/compliment/")
-        .then(res => {
-            const data = res.data;
-            alert(data);
-    });
+  axios.get("http://localhost:4000/api/compliment/").then((res) => {
+    const data = res.data;
+    alert(data);
+  });
 };
 
-complimentBtn.addEventListener('click', getCompliment)
+complimentBtn.addEventListener("click", getCompliment);
 //------------------------------------------------------------------------
 //Add a New Feature - "fortunesButton":
-const fortunesButton = document.getElementById("fortunesButton")
+const fortunesButton = document.getElementById("fortunesButton");
 
 const getFortunes = () => {
-    axios.get("http://localhost:4000/api/fortunes/")
-        .then(res => {
-            const data = res.data;
-            alert(data);
-    });
+  axios.get("http://localhost:4000/api/fortunes/").then((res) => {
+    const data = res.data;
+    alert(data);
+  });
 };
 
-fortunesButton.addEventListener('click', getFortunes)
+fortunesButton.addEventListener("click", getFortunes);
 //------------------------------------------------------------------------
 //Part 3: add 3 More Features: "Greetings Button":
-const goodGreetingsButton = document.getElementById("greetings")
+const goodGreetingsButton = document.getElementById("greetings");
 
 const getGoodGreetings = () => {
-    axios.get("http://localhost:4000/api/goodGreetings/")
-        .then(res => {
-            const data = res.data;
-            alert(data);
-        });
-}
+  axios.get("http://localhost:4000/api/goodGreetings/").then((res) => {
+    const data = res.data;
+    alert(data);
+  });
+};
 
-goodGreetingsButton.addEventListener('click', getGoodGreetings)
+goodGreetingsButton.addEventListener("click", getGoodGreetings);
 //------------------------------------------------------------------------
 //Part 3: add 3 More Features: "Input Space & Input Button":
 
 const inputSpace = document.getElementById("input_space");
 const inputButton = document.getElementById("input_btn");
 
-const handleInput = () =>{
-    console.log(inputSpace.value)
-    axios.post("http://localhost:4000/api/addUserInput/", {value: inputSpace.value})
-        .then(res => {
-            const data = res.data;
-            alert(data);
-        })
-}
+const handleInput = () => {
+  console.log(inputSpace.value);
+  axios
+    .post("http://localhost:4000/api/addUserInput/", {
+      value: inputSpace.value,
+    })
+    .then((res) => {
+      const data = res.data;
+      alert(data);
+    });
+};
 
-inputButton.addEventListener('click', handleInput)
+inputButton.addEventListener("click", handleInput);
 //------------------------------------------------------------------------
-const getAllUserInputsButton = document.getElementById("get_all_users")
+const getAllUserInputsButton = document.getElementById("get_all_users");
 
 const getAllUserInputs = () => {
-    axios.get("http://localhost:4000/api/getAllUsers/")
-        .then(res => {
-            const data = res.data;
-            alert(data);
-        });
-}
-
+  axios.get("http://localhost:4000/api/getAllUsers/").then((res) => {
+    const data = res.data;
+    alert(data);
+  });
+};
+getAllUserInputsButton.addEventListener("click", getAllUserInputs);
--- server/controller.js
+++ server/controller.js
@@ -3,53 +3,60 @@
 //the "getAllUserInputs" GET request
 const emptyArray = [];
 
-
 module.exports = {
+  getCompliment: (req, res) => {
+    const compliments = [
+      "Gee, you're a smart cookie!",
+      "Cool shirt!",
+      "Your Javascript skills are stellar.",
+    ];
 
-    getCompliment: (req, res) => {
-        const compliments = ["Gee, you're a smart cookie!", "Cool shirt!", "Your Javascript skills are stellar."];
-      
-        // choose random compliment
-        let randomIndex = Math.floor(Math.random() * compliments.length);
-        let randomCompliment = compliments[randomIndex];
-      
-        res.status(200).send(randomCompliment);
-    },
+    // choose random compliment
+    let randomIndex = Math.floor(Math.random() * compliments.length);
+    let randomCompliment = compliments[randomIndex];
 
-    //FortuNE Quotes:
-    getFortunes: (req, res) => {
-        const fortunes = ["A beautiful, smart, and loving person will be coming into your life.", "A dubious friend may be an enemy in camouflage.", 
-        "A faithful friend is a strong defense.", "A feather in the hand is better than a bird in the air.", "A fresh start will put you on your way."];
-      
-        // choose random fortunes
-        let randomIndex2 = Math.floor(Math.random() * fortunes.length);
-        let randomFortuneQuotes = fortunes[randomIndex2];
-      
-        res.status(200).send(randomFortuneQuotes);
-    },
+    res.status(200).send(randomCompliment);
+  },
 
-    //     //Add 3 More Features:1ST: Good Greeting Messages:
-    getGoodGreetings: (req, res) => {
-        const greetings = ["Hello!","Hi there.", "Good morning!", "What's Up!" ];
-      
-        // choose random greetings
-        let randomIndex3 = Math.floor(Math.random() * greetings.length);
-        let randomGoodGreetings = greetings[randomIndex3];
-      
-        res.status(200).send(randomGoodGreetings);
-    },
+  //FortuNE Quotes:
+  getFortunes: (req, res) => {
+    const fortunes = [
+      "A beautiful, smart, and loving person will be coming into your life.",
+      "A dubious friend may be an enemy in camouflage.",
+      "A faithful friend is a strong defense.",
+      "A feather in the hand is better than a bird in the air.",
+      "A fresh start will put you on your way.",
+    ];
 
-    //Add 3 More Features: 2ND - 
-    getUserInput: (req,res) => {
-        const userInput = req.body.value;
-        emptyArray.push(userInput)
+    // choose random fortunes
+    let randomIndex2 = Math.floor(Math.random() * fortunes.length);
+    let randomFortuneQuotes = fortunes[randomIndex2];
 
-        res.status(200).send(userInput)
-    },
+    res.status(200).send(randomFortuneQuotes);
+  },
 
-    //Add 3 More Features: 3RD
-    getAllUserInputs: (req, res) => {
-        console.log(emptyArray)
-        res.status(200).send(emptyArray)
-    }
-}
+  //     //Add 3 More Features:1ST: Good Greeting Messages:
+  getGoodGreetings: (req, res) => {
+    const greetings = ["Hello!", "Hi there.", "Good morning!", "What's Up!"];
+
+    // choose random greetings
+    let randomIndex3 = Math.floor(Math.random() * greetings.length);
+    let randomGoodGreetings = greetings[randomIndex3];
+
+    res.status(200).send(randomGoodGreetings);
+  },
+
+  //Add 3 More Features: 2ND -
+  getUserInput: (req, res) => {
+    const userInput = req.body.value;
+    emptyArray.push(userInput);
+
+    res.status(200).send(userInput);
+  },
+
+  //Add 3 More Features: 3RD
+  getAllUserInputs: (req, res) => {
+    console.log(emptyArray);
+    res.status(200).send(emptyArray);
+  },
+};
